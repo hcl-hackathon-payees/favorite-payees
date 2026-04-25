d9222120ced1c364921b847dacd440bf19612a82
saivarun <saivarunbandari123@gmail.com>
2026-04-25 14:15:04 +0530

    [saivarun] added swagger config and created controller test cases

--- src/main/java/com/hcl/favouritePayee/service/PayeesService.java
+++ src/main/java/com/hcl/favouritePayee/service/PayeesService.java
@@ -1,22 +1,21 @@
 package com.hcl.favouritePayee.service;
 
-
 import com.hcl.favouritePayee.dto.FavoritePayeeResponse;
 import com.hcl.favouritePayee.repository.FavoritePayeeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PayeesService {
 
-    @Autowired
-    FavoritePayeeRepository payeeRepository;
+    private final FavoritePayeeRepository repository;
 
     public Page<FavoritePayeeResponse> getFavoriteAccounts(Long customerId, Pageable pageable) {
-        return payeeRepository.findByCustomerId(customerId,pageable);
+        return repository.findByCustomerId(customerId,pageable);
     }
+
 }
