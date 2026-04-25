093afeb6555e48311ddd7fba47a3b330a763fccf
Pavan M <pavan.M@Clarivate.com>
2026-04-25 12:31:30 +0530

    Add Spring Boot starter project files

---
+++ src/test/java/com/hcl/favouritePayee/FavouritePayeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.favouritePayee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouritePayeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
